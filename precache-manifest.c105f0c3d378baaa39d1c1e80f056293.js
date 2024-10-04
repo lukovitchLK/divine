9a9b1fba2bff98fed2973109abd1365bc5cd2aac
Lucky Makola <luckymakola@Luckys-MacBook-Air.local>
2024-10-04 15:03:15 +0200

    Updates

---
+++ precache-manifest.c105f0c3d378baaa39d1c1e80f056293.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c90f6a5a2d77132a5aee80751781b1e3",
+    "url": "/divine/index.html"
+  },
+  {
+    "revision": "457105b6736b0e149d6d",
+    "url": "/divine/static/css/2.7b1f77b4.chunk.css"
+  },
+  {
+    "revision": "e76ffd3503b7db015c33",
+    "url": "/divine/static/css/main.a7f609d2.chunk.css"
+  },
+  {
+    "revision": "457105b6736b0e149d6d",
+    "url": "/divine/static/js/2.aab579af.chunk.js"
+  },
+  {
+    "revision": "73ac3e77270d47d6ddab8117c577bd21",
+    "url": "/divine/static/js/2.aab579af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e76ffd3503b7db015c33",
+    "url": "/divine/static/js/main.0507b373.chunk.js"
+  },
+  {
+    "revision": "23a3a1e2ef94c2fec547",
+    "url": "/divine/static/js/runtime-main.719409ec.js"
+  },
+  {
+    "revision": "ad4427cc3f160e2c6c701a4aac767289",
+    "url": "/divine/static/media/About.ad4427cc.png"
+  },
+  {
+    "revision": "16683949060582f54500aef6fd425e70",
+    "url": "/divine/static/media/About2.16683949.png"
+  },
+  {
+    "revision": "3d45f423d62a5020c05c928ad8772391",
+    "url": "/divine/static/media/arrow.3d45f423.svg"
+  },
+  {
+    "revision": "a74212b632dc205e2bd832acaa46a2e9",
+    "url": "/divine/static/media/arrow.a74212b6.svg"
+  },
+  {
+    "revision": "a564bf21f53d804a800e180bfc1373c1",
+    "url": "/divine/static/media/background.a564bf21.jpg"
+  },
+  {
+    "revision": "db57da795c9c521b14cf9f487da06d05",
+    "url": "/divine/static/media/dribble.db57da79.svg"
+  },
+  {
+    "revision": "aa1e8c9c8592f04d2d3d68c86a746696",
+    "url": "/divine/static/media/facebook.aa1e8c9c.svg"
+  },
+  {
+    "revision": "190e6575df7277f5c5d39fa0dfebbb0c",
+    "url": "/divine/static/media/hero.190e6575.png"
+  },
+  {
+    "revision": "f521cf4553d6d240c47e135be9ba7a9c",
+    "url": "/divine/static/media/mobile-close.f521cf45.svg"
+  },
+  {
+    "revision": "10823520de33dffb05e6963a0d8c7bec",
+    "url": "/divine/static/media/mobile-menu.10823520.svg"
+  },
+  {
+    "revision": "0ec3d3e2b99c6a27326ecff088fb4703",
+    "url": "/divine/static/media/name_logo.0ec3d3e2.png"
+  },
+  {
+    "revision": "8ce4eaffe5389aa5b28b28989eacb0c6",
+    "url": "/divine/static/media/partner01.8ce4eaff.jpg"
+  },
+  {
+    "revision": "523c620f762aa2c1527c8a0e39b170d5",
+    "url": "/divine/static/media/partner02.523c620f.jpg"
+  },
+  {
+    "revision": "a3ca386a4044b7de9fe9757139089726",
+    "url": "/divine/static/media/partner04.a3ca386a.webp"
+  },
+  {
+    "revision": "b250ba5305760dc2750f531abb88d540",
+    "url": "/divine/static/media/partner05.b250ba53.png"
+  },
+  {
+    "revision": "22fc9a82f664320bdb525b0462cfc7ad",
+    "url": "/divine/static/media/preview.22fc9a82.png"
+  },
+  {
+    "revision": "2e5f3f8ef487fb8f67008fb0f7be4eb6",
+    "url": "/divine/static/media/preview.2e5f3f8e.png"
+  },
+  {
+    "revision": "418a419966008117758672aa7d204330",
+    "url": "/divine/static/media/preview.418a4199.png"
+  },
+  {
+    "revision": "4dee8c10f35b0bbb2cef249656245a12",
+    "url": "/divine/static/media/preview.4dee8c10.png"
+  },
+  {
+    "revision": "6b945c28215409f4b8230e4e5e9bff96",
+    "url": "/divine/static/media/preview.6b945c28.png"
+  },
+  {
+    "revision": "73351b161a4250d6612012a9366d7b5b",
+    "url": "/divine/static/media/preview.73351b16.png"
+  },
+  {
+    "revision": "0ecdf21ae145f462c5258daad33c2dda",
+    "url": "/divine/static/media/twitter.0ecdf21a.svg"
+  },
+  {
+    "revision": "a0f2b83daeebc375dd391b61e6906b2d",
+    "url": "/divine/static/media/white.a0f2b83d.png"
+  }
+]);
